942c8a0db7e9cdb9c25cc2b7371681f90559ff0d
Iuli Obreja <iuliobreja23@gmail.com>
2023-07-19 21:00:10 +0300

    FE logic-exercises: exercise 4 added - V1

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ function sumNaturalNums(num) {
 
   for(let i = 1; i <= num && num <= 1000000; i++) {
     sum += i;
-  }
+  };
 
   if(num > 1000000) {
     console.log('Please, enter a number smaller than 1000000!');
-  } 
+  };
  
   return sum;
 };
@@ -26,15 +26,15 @@ console.log(sumNaturalNums(7));
 function sumEvenNums(num) {
   let sum = 0;
 
-  for(let i = 2; i <= 2 * num && num > 0 && num <= 10000; i++) {
+  for(let i = 2; i <= 2 * num && num > 0 && num <= 10000; i++) { 
     if(i % 2 === 0) {
       sum += i;
-    }
+    };
   };
 
   if(num <= 0 || num > 10000) {
     console.log('Please, enter a number smaller than 1000000!');
-  } 
+  };
  
   return sum;
 };
@@ -62,3 +62,30 @@ function nFactorial(num) {
 };
 
 console.log(nFactorial(4));
+
+
+// --- Exercise 4 ---
+// Write a program that reads the natural number n and determines the:
+// sum S=1*n+2*(n-1)+3*(n-2)+...+n*1.
+// Restrictions and clarifications: 0 < n <= 100
+
+function sumFromMultiply(num) {
+  let sum = 0;
+  let decrementor = num;
+
+  for (let i = 1; i <= num && num > 0 && num <= 100; i++) {
+    if (i > 1) {
+      sum += i * --decrementor;
+      } else {
+      sum += i * num;
+    };
+  };
+
+  if(num < 0 || num > 100) {
+    console.log('Please, enter a number between 0 and 100!');
+  };
+
+  return sum;
+};
+
+console.log(sumFromMultiply(4));
