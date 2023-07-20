f4db1a9272aea7e1edcedee61f4f4f4ddfdba105
Iuli Obreja <iuliobreja23@gmail.com>
2023-07-21 00:26:12 +0300

    FE logical-exercises: exercise 5 added - V2

--- main.js
+++ main.js
@@ -89,3 +89,30 @@ function sumFromMultiply(num) {
 };
 
 console.log(sumFromMultiply(4));
+
+
+// --- Exercise 5 ---
+// Given the nonzero natural number n. Find the product of the first n odd numbers
+// Restrictions and clarifications: 1 ≤ n ≤ 15
+
+function returnOddProduct(limit) {
+  if (limit <= 0 || limit > 15) {
+    throw Error('Please enter a number between 1 and 15!');
+  };
+  
+  let productOfOdd = 1;
+  let counterFoundOddNumbers = 0;
+  let startingPoint = 1;
+
+  while (counterFoundOddNumbers < limit) {
+    if (startingPoint % 2 === 1) {
+      counterFoundOddNumbers = counterFoundOddNumbers + 1;
+      productOfOdd = productOfOdd * startingPoint;
+    }
+    startingPoint = startingPoint + 1;
+  };
+  
+  return productOfOdd;
+};
+
+console.log(returnOddProduct(4));
