1709d68360287190bf5c42cd8ed64973443b5fce
Iuli Obreja <iuliobreja23@gmail.com>
2023-07-25 13:56:48 +0300

    FE logix-exercises: ex 13 - find smallest positive number until zero - V1

--- main.js
+++ main.js
@@ -289,3 +289,34 @@ function findMaxNumberUntilZero(...numbers) {
 
 const numbersArray4 = [4, 6, 20, 34, 0, 222];
 console.log(findMaxNumberUntilZero(...numbersArray4));
+
+
+// --- Exercise 13 ---
+// Integers are read from the keyboard until zero appears -> 
+// -> Determine the smallest among the positive values read
+// Restrictions and clarifications: 
+//      -> if there are no positive values among the read values,
+//         display the message 'NO POSITIVE VALUES FOUND'.
+
+function findSmallestPositiveValueUntilZero(...numbers) {
+  let arrayWithZeroLast = [];
+  let countPositive = 0;
+  
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+      arrayWithZeroLast.push(numbers[i]);
+      countPositive++;
+    } else if (numbers[i] === 0) {   
+      break;
+    }
+  };
+  
+  if(!countPositive) {
+      throw Error('No positive values found.');
+  };
+
+  return Math.min(...arrayWithZeroLast);
+};
+
+const numbersArray5 = [-4, 20, 8, -2, 15, 3, 0, 1];
+console.log(findSmallestPositiveValueUntilZero(...numbersArray5));
