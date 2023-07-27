91b9e37edfaab10c0e62b10f01426a229cd351fd
Iuli Obreja <iuliobreja23@gmail.com>
2023-07-27 14:29:18 +0300

    FE logic-exercises: ex-2 small modifications and comment added

--- ex-2-sum-of-first-even-numbers.js
+++ ex-2-sum-of-first-even-numbers.js
@@ -5,9 +5,7 @@
 function sumEvenNums(num, divider = 2) {
   try {
     if (num < 0 || num > 10_000) {
-      // const x = new Error();
-      // x.code = 'NUMBER_HIGHER';
-      throw {code: "NUMBER_HIGHER"};
+      throw "NUMBER_HIGHER";
     }
 
     let sum = 0;
@@ -17,9 +15,8 @@ function sumEvenNums(num, divider = 2) {
     }
 
     return sum;
-  } catch (error) {
-    // console.log(e);
-    if (error.code === 'NUMBER_HIGHER') {
+  } catch (exc) {
+    if (exc === 'NUMBER_HIGHER') {
       console.log(`Your number is higher than 10.000`);
     }
     return 0;
@@ -27,7 +24,49 @@ function sumEvenNums(num, divider = 2) {
 };
 
 console.log(sumEvenNums(20000));
-  console.log(sumEvenNums(4, 2));
-  console.log(sumEvenNums(40000));
-  console.log(sumEvenNums(4));
+console.log(sumEvenNums(4, 2));
+console.log(sumEvenNums(40000));
+console.log(sumEvenNums(4));
 console.log(sumEvenNums(30000));
+
+
+// Am invatat ca parametrii functiilor pot avea default values, atribuite
+// direct in declaratia functiei - function name(par1, par2 = value) {};
+
+// Am invatat ca "throw" nu "arunca" doar o Eroare, ci o exceptie;
+// exceptia poate fi orice, string, numar, obiect, ideea e sa 
+// ii fac handle in "catch"
+
+// Am invatat ca starting pointul pt iteratia din "for" poate fi
+// initializat direct cu param functiei-> let i = param2, iar
+// incrementorul la fel, tot pe baza parametrului funct 
+// -> i = i + param2
+
+// Am invatat ca "Try catch" e folosit cel mai bine in interiorul
+// functiei, nest-uind tot codul functiei in interiorul "Try-ului",
+// unde dam si Throw la exceptie, apoi, dupa "Try", catch-uim eroarea 
+// si o tratam
+// facem acest lucru pt a nu trata eroarea la fiecare apelare a functiei
+// respective
+/* 
+
+  function name() {
+    try {
+      if (condition) {
+        throw exception -> nr, string, obj, etc
+      }
+
+      return-ul functiei
+    } catch (exception) {
+        if (exception === ce am pus in throw) {
+          console.log(exception) sau ce vrem sa facem cu ea;
+        }
+      
+      in functie de unde avem throw, apelarea functiei poate
+      returna "undefined", asa ca dam si un return
+
+      return ce dorim, in functie de caz;
+    }
+  };
+
+*/
