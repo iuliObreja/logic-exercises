89be8f815999f629c6691c254807928b5280a431
Iuli Obreja <iuliobreja23@gmail.com>
2023-08-02 01:12:28 +0300

    FE logic-exercises: ex 6 - new recursive method added and refactored functions name

--- ex-6-sum-of-n-at-n-power.js
+++ ex-6-sum-of-n-at-n-power.js
@@ -3,7 +3,7 @@
 // sum s = 1**1 + 2**2 + ... + n**n
 // Restrictions and clarifications: 0 < n <= 15
 
-function returnSumOfExponentiatedNumbers(num) {
+function calcExponentialSum(num) {
   try { 
     if (num <= 0 || num > 15) {
       throw Error('NUMBER_SMALLER_OR_HIGHER');
@@ -24,4 +24,33 @@ function returnSumOfExponentiatedNumbers(num) {
   }
 };
 
-console.log(returnSumOfExponentiatedNumbers(2));
+console.log(calcExponentialSum(4));
+
+
+// recursive method
+function calcExponentialSumRecursive(num) {
+  if (num <= 1) {
+    return 1;
+  }
+
+  return calcExponentialSumRecursive(num - 1) + num ** num; 
+};
+
+console.log(calcExponentialSumRecursive(4));
+
+// num = 4
+// 4 <= 1 ? nu
+//  return(3) + 4**4
+
+// num = 3
+// 3 <= 1 ? nu
+//  return(2) + 3**3
+
+// num = 2
+// 2 <= 1 ? nu
+//  return(1) + 2**2
+
+// num = 1
+// 1 <= 1? dhea! => 1
+
+// return 1 + 2**2 + 3**3 + 4**4
