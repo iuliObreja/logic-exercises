2e0148d09bf91b42529b303103d7e7a32633e2a5
Iuli Obreja <iuliobreja23@gmail.com>
2023-08-04 01:59:55 +0300

    FE logic-exercises: homework assignment updated for ex 10

--- ex-10-calculate-min-number-of-n-numbers.js
+++ ex-10-calculate-min-number-of-n-numbers.js
@@ -11,8 +11,10 @@ const numbersArray2 = [21, 6, 9, 4, 23, 17, 14, 26, 28];
 const numbersArray3 = [22, 16, 4, -5, 3, 12, 19, 31, 11];
 const numbersArray4 = [12, 9, 4, 18, 2, -1, 6, 51, 33, 33];
 const numbersArray5 = [3, 66, 72, 13, 21, 67, 9, 16, 23];
-console.log(returnMinNumber(numbersArray2));
+const numbersArray6 = [-2, -6, -4, -100, -23, -167, -9, -5, -3];
+const numbersArray7 = [-12, -9, -4, -18, -2, -1, -6, -51, -33, 33];
 
+console.log(returnMinNumber(numbersArray2));
 // Haumworc
 
 
