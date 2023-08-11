f4595acceacf6d52f0f301ea4de75c59fd9550b4
Iuli Obreja <iuliobreja23@gmail.com>
2023-08-11 16:05:28 +0300

    FE logic-exercises: ex 14 - bug FIXED

--- ex-14-calculate-max-negative-number.js
+++ ex-14-calculate-max-negative-number.js
@@ -9,7 +9,7 @@ function calculateMaxNegativeNumber(numbers) {
       } 
     }
     
-    let maxRealNum = negativeNumbersArray[0];
+    let maxNegNum = negativeNumbersArray[0];
     if (negativeNumbersArray[0] === undefined) {
       throw `NO_NEGATIVE_NUMBER`;
     }
@@ -19,8 +19,8 @@ function calculateMaxNegativeNumber(numbers) {
     }
     
     for (let i = 1; i < negativeNumbersArray.length; i++) {
-      if (maxRealNum < negativeNumbersArray[i]) {
-        maxRealNum = negativeNumbersArray[i];
+      if (maxNegNum < negativeNumbersArray[i]) {
+        maxNegNum = negativeNumbersArray[i];
       }
     }
     
@@ -35,15 +35,15 @@ function calculateMaxNegativeNumber(numbers) {
 const numbersArray1 = [2, 6, 4, 100, 23, 167, 9, 5, 3];
 const numbersArray2 = [21, 6, 9, 4, 23, 17, 14, 26, 28];
 const numbersArray3 = [22, 16, 4, -5, 3, 12, 19, 31, 11];
-const numbersArray4 = [12, 9, 4, 18, 2, -1, 6, 51, 33, 33];
+const numbersArray4 = [12, 9, 4, -2, 18, 2, -7, 6, 51, 33, 33];
 const numbersArray5 = [3, 66, 72, 13, 21, 67, 9, 16, 23];
 const numbersArray6 = [-2, -6, -4, -100, -23, -167, -9, -5, -3];
 const numbersArray7 = [-12, -9, -4, -18, -2, -1, -6, -51, -33, 33];
 
-console.log(calculateMaxRealNumber(numbersArray1));
-console.log(calculateMaxRealNumber(numbersArray2));
-console.log(calculateMaxRealNumber(numbersArray3));
-console.log(calculateMaxRealNumber(numbersArray4));
-console.log(calculateMaxRealNumber(numbersArray5));
-console.log(calculateMaxRealNumber(numbersArray6));
-console.log(calculateMaxRealNumber(numbersArray7));
+console.log(calculateMaxNegativeNumber(numbersArray1));
+console.log(calculateMaxNegativeNumber(numbersArray2));
+console.log(calculateMaxNegativeNumber(numbersArray3));
+console.log(calculateMaxNegativeNumber(numbersArray4));
+console.log(calculateMaxNegativeNumber(numbersArray5));
+console.log(calculateMaxNegativeNumber(numbersArray6));
+console.log(calculateMaxNegativeNumber(numbersArray7));
